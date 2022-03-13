37283bef335e3e1e0a5b4ec0f3592336d74f8fac
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2022-03-13 13:37:49 -0300

    feat: add user service

---
+++ services/user.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gabriel1680/go-grpc/pb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+
+	// saving in database...
+	fmt.Println(req.Email)
+
+	return &pb.UserResponse{
+		Id:       "someBigAndUniqueId",
+		FullName: req.GetFirstName() + "" + req.GetLastName(),
+		Email:    req.GetEmail(),
+	}, nil
+
+}
